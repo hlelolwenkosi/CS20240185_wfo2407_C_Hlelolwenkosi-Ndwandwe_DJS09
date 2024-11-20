064718eedb7a1a564ac218432ea1d1740a051939
Hlelolwenkosi Ndwandwe <1071hf@gmail.com>
2024-11-20 13:40:16 +0200

    "Added type annotations to reviews array and created showReviewTotal function to display review count"

--- index.ts
+++ index.ts
@@ -1,26 +1,39 @@
 // Number Types mini-challenge 10 10.2
 // Write a function that will only accept numbers and attend to 
 // all TypeScript weakness flags.
-// : number
-const reviewTotalDisplay = document.querySelector('#reviews')
 
-const reviews = [
-    {
-        name: 'Sheia',
-        stars: 5,
-        loyaltyUser: true,
-        date: '01-04-2021'
-    },
-    {
-        name: 'Andrzej',
-        stars: 3,
-        loyaltyUser: false,
-        date: '28-03-2021'
-    },
-    {
-        name: 'Omar',
-        stars: 4,
-        loyaltyUser: true,
-        date: '27-03-2021'
-    },
-]
+const reviewTotalDisplay = document.querySelector('#reviews');
+
+const reviews: {
+  name: string;
+  stars: number;
+  loyaltyUser: boolean;
+  date: string;
+}[] = [
+  {
+      name: 'Sheia',
+      stars: 5,
+      loyaltyUser: true,
+      date: '01-04-2021'
+  },
+  {
+      name: 'Andrzej',
+      stars: 3,
+      loyaltyUser: false,
+      date: '28-03-2021'
+  },
+  {
+      name: 'Omar',
+      stars: 4,
+      loyaltyUser: true,
+      date: '27-03-2021'
+  }
+];
+
+function showReviewTotal(value: number) {
+  if (reviewTotalDisplay) {
+      reviewTotalDisplay.innerHTML = "review total " + value.toString();
+  }
+}
+
+showReviewTotal(reviews.length);
