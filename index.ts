ca56aaed4724dd4c9076c033f5791766491234ea
Hlelolwenkosi Ndwandwe <1071hf@gmail.com>
2024-11-20 15:31:50 +0200

    refactored user data to properties array and removed user object then updated function call

--- index.ts
+++ index.ts
@@ -44,24 +44,58 @@ const reviews: {
     }
 }showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
 
-const you: {
-    firstName: string;
-    lastName: string;
-    isReturning: boolean;
-    age: number;
-    stayedAt: string[];
-  } = {
-    firstName: "Bobby",
-    lastName: "Brown",
-    isReturning: true,
-    age: 35,
-    stayedAt: ["florida-home", "oman-flat", "tokyo-bungalow"],
+const properties: {
+  image: string;
+  title: string;
+  price: number;
+  location: {
+    firstLine: string;
+    city: string;
+    code: number;
+    country: string;
   };
+  contact: string;
+  isAvailable: boolean;
+  image: "",
+  title: "Colombian Shack",
+  price: 45,
+  location: {
+    firstLine: "shack 37",
+    city: "Bogota",
+    code: 45632,
+    country: "Colombia",
+  },
+  contact: "marywinkle@gmail.com",
+  isAvailable: true,
+},
+{
+  image: "",
+  title: "Polish Cottage",
+  price: 34,
+  location: {
+    firstLine: "no 23",
+    city: "Gdansk",
+    code: 343903,
+    country: "Poland",
+  },
+  contact: "garydavis@hotmail.com",
+  isAvailable: false,
+},
+{
+  image: "",
+  title: "London Flat",
+  price: 23,
+  location: {
+    firstLine: "flat 15",
+    city: "London",
+    code: 35433,
+    country: "United Kingdom",
+  },
+  contact: "andyluger@aol.com",
+  isAvailable: true,
+},
+];
+}[] 
 
-    function populateUser(isReturning: boolean, userName: string) {
-        if (isReturning) {
-          returningUserDisplay.innerHTML = "back";
-        }
-        userNameDisplay.innerHTML = userName;
-      }
+
       populateUser(you.isReturning, you.firstName);
