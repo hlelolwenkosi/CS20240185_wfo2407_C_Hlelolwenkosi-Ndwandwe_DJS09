4216615a1acabb9a973da1ef80a9fda3067c0a42
Hlelolwenkosi Ndwandwe <1071hf@gmail.com>
2024-11-20 15:16:23 +0200

    Remove variable declarations and add import statement

--- index.ts
+++ index.ts
@@ -2,10 +2,7 @@
 // Write a function that will only accept numbers and attend to 
 // all TypeScript weakness flags.
 
-const reviewTotalDisplay = document.querySelector("#reviews");
-const returningUserDisplay = document.querySelector("#returning-user");
-const userNameDisplay = document.querySelector("#user");
-const age = 23;
+import { showReviewTotal, populateUser } from "./utils";
 let isOpen: boolean;
 
 const reviews: {
