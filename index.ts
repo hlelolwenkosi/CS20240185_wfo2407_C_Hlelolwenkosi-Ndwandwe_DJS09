43029dd86000497a292e6e66a242d3c706b73363
Hlelolwenkosi Ndwandwe <1071hf@gmail.com>
2024-11-21 06:27:02 +0200

    added content on the index.ts and utils.ts files

--- index.ts
+++ index.ts
@@ -1,112 +1,168 @@
-// Number Types mini-challenge 10 10.2
-// Write a function that will only accept numbers and attend to 
-// all TypeScript weakness flags.
+import "../index.css"
 
-import { showReviewTotal, populateUser } from "./utils";
-let isOpen: boolean;
+import {
+  showReviewTotal,
+  populateUser,
+  showDetails,
+  getTopTwoReviews,
+} from "./utils";
+import { Permissions, LoyaltyUser } from "./enums";
+import { Review, Property } from "./interfaces";
+import { MainProperty } from "./classes";
+const propertyContainer = document.querySelector(".properties");
+const reviewContainer = document.querySelector(".reviews");
+const container = document.querySelector(".container");
+const button = document.querySelector("button");
+const footer = document.querySelector(".footer");
 
-const reviews: {
-  name: string;
-  stars: number;
-  loyaltyUser: boolean;
-  date: string;
-}[] = [
+let isLoggedIn: boolean;
+
+// Reviews
+const reviews: Review[] = [
   {
-      name: 'Sheia',
-      stars: 5,
-      loyaltyUser: true,
-      date: '01-04-2021'
+    name: "Sheila",
+    stars: 5,
+    loyaltyUser: LoyaltyUser.GOLD_USER,
+    date: "01-04-2021",
   },
   {
-      name: 'Andrzej',
-      stars: 3,
-      loyaltyUser: false,
-      date: '28-03-2021'
+    name: "Andrzej",
+    stars: 3,
+    loyaltyUser: LoyaltyUser.BRONZE_USER,
+    date: "28-03-2021",
   },
   {
-      name: 'Omar',
-      stars: 4,
-      loyaltyUser: true,
-      date: '27-03-2021'
-  }
+    name: "Omar",
+    stars: 4,
+    loyaltyUser: LoyaltyUser.SILVER_USER,
+    date: "27-03-2021",
+  },
 ];
 
- {function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
-    const iconDisplay = isLoyalty ? "⭐" : "";
-    if (reviewTotalDisplay) {
-        reviewTotalDisplay.innerHTML =
-        "review total" +
-        value.toString() +
-        "| last reviewed by " +
-        reviewer +
-        "" +
-        iconDisplay;  
+const you = {
+  firstName: "Bobby",
+  lastName: "Brown",
+  permissions: Permissions.ADMIN,
+  isReturning: true,
+  age: 35,
+  stayedAt: ["florida-home", "oman-flat", "tokyo-bungalow"],
+};
+
+// Array of  Properties
+const properties: Property[] = [
+  {
+    image: "images/colombia-property.jpg",
+    title: "Colombian Shack",
+    price: 45,
+    location: {
+      firstLine: "shack 37",
+      city: "Bogota",
+      code: 45632,
+      country: "Colombia",
+    },
+    contact: [+1123495082908, "marywinkle@gmail.com"],
+    isAvailable: true,
+  },
+  {
+    image: "images/poland-property.jpg",
+    title: "Polish Cottage",
+    price: 30,
+    location: {
+      firstLine: "no 23",
+      city: "Gdansk",
+      code: 343903,
+      country: "Poland",
+    },
+    contact: [+1123495082908, "garydavis@hotmail.com"],
+    isAvailable: false,
+  },
+  {
+    image: "images/london-property.jpg",
+    title: "London Flat",
+    price: 25,
+    location: {
+      firstLine: "flat 15",
+      city: "London",
+      code: "SW4 5XW",
+      country: "United Kingdom",
+    },
+    contact: [+1123495082908, "andyluger@aol.com"],
+    isAvailable: true,
+  },
+  {
+    image: "images/malaysian-hotel.jpeg",
+    title: "Malia Hotel",
+    price: 35,
+    location: {
+      firstLine: "Room 4",
+      city: "Malia",
+      code: 45334,
+      country: "Malaysia",
+    },
+    contact: [+60349822083, "lee34@gmail.com"],
+    isAvailable: false,
+  },
+];
+
+// Functions
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+
+populateUser(you.isReturning, you.firstName);
+
+// Adds the properties
+for (let i = 0; i < properties.length; i++) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = properties[i].title;
+  const image = document.createElement("img");
+  image.setAttribute("src", properties[i].image);
+  card.appendChild(image);
+  showDetails(you.permissions, card, properties[i].price);
+  propertyContainer.appendChild(card);
+}
+
+// Adds reviews to the page
+let count = 0;
+function addReviews(array: Review[]): void {
+  if (!count) {
+    count++;
+    const topTwo = getTopTwoReviews(array);
+    for (let i = 0; i < topTwo.length; i++) {
+      const card = document.createElement("div");
+      card.classList.add("review-card");
+      card.innerHTML = topTwo[i].stars + " stars from " + topTwo[i].name;
+      reviewContainer.appendChild(card);
     }
-}showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+    container.removeChild(button);
+  }
+}
 
-const properties: {
-  image: string;
-  title: string;
-  price: number;
-  location: {
-    firstLine: string;
-    city: string;
-    code: number;
-    country: string;
-  };
-  contact: string;
-  isAvailable: boolean;
-  image: "",
-  title: "Colombian Shack",
-  price: 45,
-  location: {
-    firstLine: "shack 37",
-    city: "Bogota",
-    code: 45632,
-    country: "Colombia",
-  },
-  contact: "marywinkle@gmail.com",
-  isAvailable: true,
-},
-{
-  image: "",
-  title: "Polish Cottage",
-  price: 34,
-  location: {
-    firstLine: "no 23",
-    city: "Gdansk",
-    code: 343903,
-    country: "Poland",
-  },
-  contact: "garydavis@hotmail.com",
-  isAvailable: false,
-},
-{
-  image: "",
-  title: "London Flat",
-  price: 23,
-  location: {
-    firstLine: "flat 15",
-    city: "London",
-    code: 35433,
-    country: "United Kingdom",
-  },
-  contact: "andyluger@aol.com",
-  isAvailable: true,
-},
-];
-}[] 
+button.addEventListener("click", () => addReviews(reviews));
 
+// Replaced with my location, current time and current temperature
+let currentLocation: [string, string, number] = ["Rustenburg", "13.27", 17];
+footer.innerHTML =
+  currentLocation[0] +
+  " " +
+  currentLocation[1] +
+  " " +
+  currentLocation[2] +
+  "°C";
 
-      populateUser(you.isReturning, you.firstName);
+let yourMainProperty = new MainProperty(
+  "images/italian-property.jpg",
+  "Italian House",
+  [
+    {
+      name: "Olive",
+      stars: 5,
+      loyaltyUser: LoyaltyUser.GOLD_USER,
+      date: "12-04-2021",
+    },
+  ]
+);
 
-      for (let i = 0; i < properties.length; i++) {
-        const card = document.createElement("div");
-        card.classList.add("card");
-        card.innerHTML = properties[i].title;
-        const image = document.createElement("img");
-        image.setAttribute("src", properties[i].image);
-        card.appendChild(image);
-      propertyContainer.appendChild(card);
-      }
-      
+const mainImageContainer = document.querySelector(".main-image");
+const image = document.createElement("img");
+image.setAttribute("src", yourMainProperty.src);
+mainImageContainer.appendChild(image);
--- utils.ts
+++ utils.ts
@@ -1,25 +1,53 @@
 
+
 const reviewTotalDisplay = document.querySelector("#reviews");
 const returningUserDisplay = document.querySelector("#returning-user");
 const userNameDisplay = document.querySelector("#user");
+import { LoyaltyUser, Permissions } from "./enums";
+import { Review } from "./interfaces";
 
 export function showReviewTotal(
   value: number,
   reviewer: string,
-  isLoyalty: boolean
-) {
-  const iconDisplay = isLoyalty ? "⭐" : "";
+  isLoyalty: LoyaltyUser
+): void {
+  const iconDisplay = LoyaltyUser.GOLD_USER ? "⭐" : "";
   reviewTotalDisplay.innerHTML =
-    "review total" +
     value.toString() +
+    " review" +
+    makeMultiple(value) +
     "| last reviewed by " +
     reviewer +
     "" +
     iconDisplay;
 }
+
 export function populateUser(isReturning: boolean, userName: string) {
-  if (isReturning) {
+  if (isReturning == true) {
     returningUserDisplay.innerHTML = "back";
   }
   userNameDisplay.innerHTML = userName;
+}
+
+export function showDetails(
+  value: boolean | Permissions,
+  element: HTMLDivElement,
+  price: number
+) {
+  if (value) {
+    const priceDisplay = document.createElement("div");
+    priceDisplay.innerHTML = price.toString() + "/night";
+    element.appendChild(priceDisplay);
+  }
+}
+
+export function makeMultiple(value: number): string {
+  if (value > 1 || value == 0) {
+    return "s";
+  } else return "";
+}
+
+export function getTopTwoReviews(reviews: Review[]): Review[] {
+  const sortedReviews = reviews.sort((a, b) => b.stars - a.stars);
+  return sortedReviews.slice(0, 2);
 }
